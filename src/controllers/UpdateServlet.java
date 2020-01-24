7b4a5061e1518192c8a42dedf51d8fb7cc2eb960
hasunuma <mai.hasunuma@weserve.co.jp>
2020-01-24 13:27:20 +0900

    Add UpdateServlet.java

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからタスクIDを取得して該当のIDのタスク１件のみを取得
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime);
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上のふようになったデータを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
